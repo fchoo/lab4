56afc13d9cd0131fd7874fef76f181c695157fc5
wull2think <wull2think@gmail.com>
2014-11-22 13:33:52 -0300

    added c irq handler

---
+++ kernel/c_irq_handler.c
@@ -0,0 +1,37 @@
+/*
+ * kernel.c: Kernel main (entry) function
+ *
+ * Authors: Foo Lai Choo <fchoo@andrew.cmu.edu>
+ *          Hui Jun Tay <htay@andrew.cmu.edu>
+ * Date:    Tues, 11 Nov 2014 01:51:29 -0400
+ */
+
+#include <exports.h>
+#include <types.h>
+#include <arm/reg.h>
+#include <arm/timer.h>
+#include <arm/interrupt.h>
+
+#include <globals.h>
+
+#include <config.h>
+
+
+/* C_IRQ_Handler */
+void C_IRQ_Handler() {
+	printf("ENTER C IRQ HANDLER!\n");	
+    // Get OSMR0 bit
+    uint32_t bit_OSMR0 = 1 << (INT_OSTMR_0);
+    uint32_t has_OSMR0 = reg_read(INT_ICPR_ADDR) & bit_OSMR0;
+    uint32_t next_match;
+    // if OMSR0 is set
+    if (has_OSMR0 == bit_OSMR0) {
+        // Current count + timer res = next match point
+        next_match = reg_read(OSTMR_OSCR_ADDR) + OS_TIMER_RESOLUTION;
+        reg_write(OSTMR_OSMR_ADDR(0), next_match);
+        // Clear OSSR match
+        reg_set(OSTMR_OSSR_ADDR, OSTMR_OSSR_M0);
+        // Update system time
+        systime++;
+    }
+}
---
+++ kernel/include/c_irq_handler.h
@@ -0,0 +1,14 @@
+/*
+ * irq_handler.h: Defines syscall numbers used in IRQ instructions
+ *
+ * Author: Hui Jun Tay <htay@andrew.cmu.edu>
+ *         Foo Lai Choo <fchoo@andrew.cmu.edu>
+ * Date:   Tue, 11 Nov 2014 11:20:33 -0400
+ */
+
+#ifndef C_IRQ_HANDLER_H
+#define C_IRQ_HANDLER_H
+
+void C_IRQ_Handler();
+
+#endif /* C_IRQ_HANDLER_H */
