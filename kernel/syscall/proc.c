543abe282dd7a117b6977dd88425e5b5c40a4374
Foo Lai Choo <choofoolai@gmail.com>
2014-11-26 00:16:43 -0500

    Implemented mutex init and create

--- kernel/syscall/proc.c
+++ kernel/syscall/proc.c
@@ -39,7 +39,7 @@ int task_create(task_t* tasks, size_t num_tasks)
     // Create tasks in kernel
     if (!allocate_tasks(sorted_tasks, num_tasks)) return -EFAULT;
 
-    // Start device interrupt timer
+    // Update device match time
     dev_init(systime);
     // Start the highest priority task
     dispatch_nosave();
