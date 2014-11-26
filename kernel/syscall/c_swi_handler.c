fd82e332a0b3a3cd911565955df0b1ba9c43cab4
Foo Lai Choo <choofoolai@gmail.com>
2014-11-25 20:48:51 -0500

    Implemented mutex wrappers

--- kernel/syscall/c_swi_handler.c
+++ kernel/syscall/c_swi_handler.c
@@ -13,18 +13,19 @@
 
 #include <globals.h>
 #include <syscall.h>
+#include <lock.h>
 
 /* C_SWI_Handler uses SWI number to call the appropriate function. */
 ssize_t C_SWI_Handler(int swiNum, int *regs) {
-    ssize_t count = 0;
+    ssize_t retVal = 0;
     switch (swiNum) {
         // ssize_t read(int fd, void *buf, size_t count);
         case READ_SWI:
-            count = read_syscall(regs[0], (void *) regs[1], (size_t) regs[2]);
+            retVal = read_syscall(regs[0], (void *) regs[1], (size_t) regs[2]);
             break;
         // ssize_t write(int fd, const void *buf, size_t count);
         case WRITE_SWI:
-            count = write_syscall((int) regs[0], (void *) regs[1], (size_t) regs[2]);
+            retVal = write_syscall((int) regs[0], (void *) regs[1], (size_t) regs[2]);
             break;
         // void sleep(unsigned long millis)
         case SLEEP_SWI:
@@ -32,27 +33,33 @@ ssize_t C_SWI_Handler(int swiNum, int *regs) {
             break;
 	    // unsigned long time()
         case TIME_SWI:
-            count = (ssize_t)time_syscall();
+            retVal = (ssize_t)time_syscall();
             break;
         // int task_create(task_t* tasks, size_t num_tasks)
         case CREATE_SWI:
-            count = task_create((task_t*)regs[0], (size_t)regs[1]);
+            retVal = task_create((task_t*)regs[0], (size_t)regs[1]);
             break;
         // int event_wait(unsigned int dev_num)
         case EVENT_WAIT:
-            count = event_wait((unsigned int)regs[0]);
+            retVal = event_wait((unsigned int)regs[0]);
             break;
+        // int mutex_create(void);
         case MUTEX_CREATE:
+            retVal = mutex_create();
             break;
+        // int mutex_lock(int mutex);
         case MUTEX_LOCK:
+            retVal = mutex_lock(regs[0]);
             break;
+        // int mutex_unlock(int mutex);
         case MUTEX_UNLOCK:
+            retVal = mutex_unlock(regs[0]);
             break;
-
+        // Invalid swi num
         default:
 	    invalid_syscall(swiNum);
     }
 
-    return count;
+    return retVal;
 }
 
