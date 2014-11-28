963fd2c8759bf430efcbe3103db11f4f780c811c
wull2think <wull2think@gmail.com>
2014-11-28 13:30:53 -0300

    updated inversion test w/ mid prio task

--- tasks/inversion/inversion.c
+++ tasks/inversion/inversion.c
@@ -24,7 +24,6 @@ void fun1(void* str) {
 	while (1) {
 		putchar((int)'#');
         	mutex_lock(mutex);
-		sleep(10);
 
 		putchar((int)str);
 
@@ -55,26 +54,40 @@ void fun2(void* str) {
 	}
 }
 
+void fun3(void* str) {
+	while(1) {
+
+		sleep(10);
+		putchar((int)str);
+
+		if (event_wait(1) < 0) panic("Dev 1 failed");
+	}
+}
 int main(int argc __attribute((unused)),
          char** argv __attribute((unused)))
 {
-	task_t tasks[2];
+	task_t tasks[3];
 	tasks[0].lambda = fun1;
 	tasks[0].data = (void*)'@';
 	tasks[0].stack_pos = (void*)0xa2000000;
-	tasks[0].C = 25;
+	tasks[0].C = 1;
 	tasks[0].B = 0;
 	tasks[0].T = PERIOD_DEV0;
 	tasks[1].lambda = fun2;
 	tasks[1].data = (void*)'<';
 	tasks[1].stack_pos = (void*)0xa1000000;
-	tasks[1].C = 150;
+	tasks[1].C = 165;
     	tasks[1].B = 100;
 	tasks[1].T = PERIOD_DEV2;
-
+	tasks[2].lambda = fun3;
+	tasks[2].data = (void*)'!';
+	tasks[2].stack_pos = (void*)0xa1800000;
+	tasks[2].C = 10;
+    	tasks[2].B = 100;
+	tasks[2].T = PERIOD_DEV1;
     mutex = mutex_create();
 
-	task_create(tasks, 2);
+	task_create(tasks, 3);
 	puts("Why did your code get here!\n");
 	return 0;
 }
